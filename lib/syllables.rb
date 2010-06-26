63efa812b970e538ca2bbb6156f549c24c6c52c4
James Robertson <james@r0bertson.co.uk>
2010-06-26 14:26:42 +0100

    first commit

---
+++ lib/syllables.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/ruby
+
+# file: syllables.rb
+# original code: http://stackoverflow.com/questions/1271918/ruby-count-syllables
+
+Tokenizer = /([aeiouy]{1,3})/
+
+class Syllables
+
+  def initialize(text)
+    super()
+    @words = text.gsub(/\W+/, ' ').split\
+      .inject({}) {|r,word| r.merge({word.to_sym => count_syllables(word)})}
+  end
+
+  def to_h
+    @words
+  end
+
+  private
+
+  def count_syllables(word)
+
+    len = 0
+
+    if word[-3..-1] == 'ing' then
+      len += 1
+      word = word[0...-3]
+    end
+
+    got = word.scan(Tokenizer)
+    len += got.size()
+
+    if got.size() > 1 and got[-1] == ['e'] and
+        word[-1].chr() == 'e' and
+        word[-2].chr() != 'l' then
+      len -= 1
+    end
+
+    return len
+
+  end
+end
---
+++ syllables.gemspec
@@ -0,0 +1,6 @@
+Gem::Specification.new do |s|
+  s.name = 'syllables'
+  s.version = '0.1.0'
+  s.summary = 'syllables'
+  s.files = Dir['lib/**/*.rb']
+end
